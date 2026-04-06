767f15918f5e02c3bc17198503218c50ef9c9e0c
manoranjaan-cpp <manoranjaan.mr@gmail.com>
2026-04-06 10:45:30 +0530

    final version

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -4,20 +4,30 @@ import org.junit.jupiter.api.Test;
 public class AppTest {
 
     @Test
-    void testNormalFare() {
+    void testNonPeakNormalDemand() {
         double fare = App.calculateFare(10, false, 1.0);
+        // 50 + (10*10) = 150
         assertEquals(150.0, fare);
     }
 
     @Test
-    void testPeakTimeFare() {
+    void testPeakTime() {
         double fare = App.calculateFare(10, true, 1.0);
+        // 150 * 1.5 = 225
         assertEquals(225.0, fare);
     }
 
     @Test
-    void testDemandFare() {
+    void testHighDemand() {
         double fare = App.calculateFare(10, false, 1.5);
+        // 150 * 1.5 = 225
         assertEquals(225.0, fare);
+    }
+
+    @Test
+    void testPeakAndHighDemand() {
+        double fare = App.calculateFare(10, true, 1.5);
+        // 150 * 1.5 * 1.5 = 337.5
+        assertEquals(337.5, fare);
     }
 }
